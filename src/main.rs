dd7fd5a4072e4eb71cb4538247d25f19f7e28f03
bokken <negibokken@gmail.com>
2022-12-05 08:04:18 +0900

    Split server and main

--- src/main.rs
+++ src/main.rs
@@ -1,62 +1,18 @@
-use tokio::{io::AsyncReadExt, io::AsyncWriteExt, net::TcpListener};
+use std::net::UdpSocket;
 
-#[tokio::main]
-async fn main() -> Result<(), Box<dyn std::error::Error>> {
-    let listener = TcpListener::bind("127.0.0.1:8000").await?;
+const BUF_MAXSIZE: usize = 1500;
 
-    loop {
-        let (mut socket, _) = listener.accept().await?;
-        tokio::spawn(async move {
-            let mut buf = [0; 1024];
-            loop {
-                let n = match socket.read(&mut buf).await {
-                    Ok(n) if n == 0 => return,
-                    Ok(n) => n,
-                    Err(e) => {
-                        eprintln!("failed to read from socket; err = {:?}", e);
-                        return;
-                    }
-                };
-
-                if let Err(e) = socket.write_all(&buf[0..n]).await { eprint!("failed to write to socket; err = {:?}", e);
-                    return;
-                }
-            }
-        });
-    }
+fn start_server() -> std::io::Result<()> {
+    let socket = UdpSocket::bind("[::]:8080")?;
+    let mut buf = [0; BUF_MAXSIZE];
+    let (amt, src) = socket.recv_from(&mut buf)?;
+    socket.connect(&src)?;
+    let buf = &mut buf[..amt];
+    buf.reverse();
+    socket.send_to(buf, &src)?;
+    Ok(())
 }
 
-#[derive(Debug,Copy,Clone,PartialEq,Eq)]
-enum StreamState {
-    Ready,
-    Send,
-    DataSent,
-    ResetSent,
-    DataRecvd,
-    ResetRecvd,
-}
-
-struct Frame {
-    typ: u8,
-    field: u8,
-}
-
-struct Payload {
-    frame: Frame,
-}
-
-struct Packet{
-    number: u64,
-    payload: Payload,
-}
-
-
-#[cfg(test)]
-mod tests {
-    use crate::StreamState;
-
-    fn foobar() {
-        let s = StreamState::DataRecvd;
-        println!("{:?}",s);
-    }
+fn main() -> std::io::Result<()> {
+    start_server()
 }
