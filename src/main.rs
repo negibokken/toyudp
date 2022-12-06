718e0cdece30b8e2014561398ad8b0f92a0aa5f1
bokken <negibokken@gmail.com>
2022-12-06 23:25:51 +0900

    Clean up

--- src/main.rs
+++ src/main.rs
@@ -2,14 +2,9 @@ use udp::server;
 
 mod udp;
 
-
 fn main() -> std::io::Result<()> {
     let mut server = server::Server::new("127.0.0.1:8080");
     let t = server.run();
     t.join();
-        // let sock = conn.socket.clone();
-        // let recv_buf = &mut recv_buf[..amt];
-        // println!("{:?}", recv_buf);
-    // }
     Ok(())
 }
