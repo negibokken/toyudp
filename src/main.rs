dd60a96e36edfa860a3cd0274f76890d0045c2d6
bokken <negibokken@gmail.com>
2022-12-06 19:15:38 +0900

    Update directory structure

--- src/main.rs
+++ src/main.rs
@@ -1,18 +1,5 @@
-use std::net::UdpSocket;
+mod udp;
 
-const BUF_MAXSIZE: usize = 1500;
-
-fn start_server() -> std::io::Result<()> {
-    let socket = UdpSocket::bind("[::]:8080")?;
-    let mut buf = [0; BUF_MAXSIZE];
-    let (amt, src) = socket.recv_from(&mut buf)?;
-    socket.connect(&src)?;
-    let buf = &mut buf[..amt];
-    buf.reverse();
-    socket.send_to(buf, &src)?;
-    Ok(())
-}
-
-fn main() -> std::io::Result<()> {
-    start_server()
+fn main() {
+    println!("hello world!");
 }
---
+++ src/udp/packet.rs
@@ -0,0 +1,3 @@
+pub struct Packet {
+    pub data: vec![u8],
+}
